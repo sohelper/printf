4161ebb5c943e23e5f0fa63658571a61cc082882
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-22 18:27:06 -0700

    doing our best

--- _putchar.c
+++ _putchar.c
@@ -7,5 +7,24 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static int buffer_counter;
+	static char buffer[1024];
+
+	if (c == -1)
+	{
+		buffer_counter = 0;
+		return (0);
+	}
+	if (c == -2 || buffer_counter == 1024)
+	{
+		write(1, buffer, buffer_counter);
+		buffer_counter = 0;
+	}
+	if (c != -1 && c != -2)
+	{
+		buffer[buffer_counter] = c;
+		buffer_counter++;
+		return (1);
+	}
+	return (0);
 }
