ddfafb4d1c05db2434517f0994da4ea4217dc1df
root <saharbadr2508@gmail.com>
2023-07-23 03:56:11 -0700

    testing things up

--- print_int.c
+++ print_int.c
@@ -1,51 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "main.h"
 /**
- * print_int_helper - instead of intoa
- * @n: number
- * @count: the count
- * @negative: test if negative
- * Return: ha
- */
-void print_int_helper(int n, int *count, int negative)
-{
-	if (n < 10)
-	{
-		if (negative)
-		{
-			_putchar('-');
-			(*count)++;
-		}
-		_putchar(n + '0');
-		(*count)++;
-		return;
-	}
-	print_int_helper(n / 10, count, negative);
-	_putchar((n % 10) + '0');
-	(*count)++;
-}
-/**
- * print_int - print the intigers
- * @args: argument list
+ * print_int - prints integer
+ * @args: list of arguments
  * @count: pointer to the count of chars
+ * Return: void
  */
 void print_int(va_list args, int *count)
 {
-	int num = va_arg(args, int);
-	int negative = 0;
+	int n = va_arg(args, int);
+	int digit;
+	int length = 0;
+	int num = n;
+	int pow = 1;
+	int i;
 
-	if (num < 0)
-	{
-		negative = 1;
-		num = -num;
-	}
-
-	if (num == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		(*count)++;
+		return;
 	}
-	else
+	if (n < 0)
 	{
-		print_int_helper(num, count, negative);
+		_putchar('-');
+		(*count)++;
+	}
+	while (num != 0)
+	{
+		num /= 10;
+		length++;
+	}
+	for (i = 1; i <= length - 1; i++)
+		pow *= 10;
+	for (i = 1; i <= length; i++)
+	{
+		digit = n / pow;
+		if (n < 0)
+		{
+			_putchar((digit * -1) + '0');
+		}
+		else
+			_putchar(digit + '0');
+		(*count)++;
+		n -= digit * pow;
+		pow /= 10;
 	}
 }
