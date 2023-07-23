8e1f1d9c054e2fb98ecac393a7add45349170826
drahmedmosaed <dr.ahmed.mosaed@gmail.com>
2023-07-22 18:51:52 -0700

    fixing buffer issue

--- _putchar.c
+++ _putchar.c
@@ -7,24 +7,5 @@
  */
 int _putchar(char c)
 {
-	static int buffer_counter;
-	static char buffer[1024];
-
-	if (c == -1)
-	{
-		buffer_counter = 0;
-		return (0);
-	}
-	if (c == -2 || buffer_counter == 1024)
-	{
-		write(1, buffer, buffer_counter);
-		buffer_counter = 0;
-	}
-	if (c != -1 && c != -2)
-	{
-		buffer[buffer_counter] = c;
-		buffer_counter++;
-		return (1);
-	}
-	return (0);
+	return (write(1, &c, 1));
 }
